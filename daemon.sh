60ca8e47dbeb4ef047f1cdd7cb5f9929ebd958c5
John Murowaniecki <joao.santanna@foxbit.com.br>
2022-03-04 11:13:28 -0300

    enhance flow

--- daemon.sh
+++ daemon.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-sudo -- bash -c '
-    echo "nameserver 1.1.1.1" > /etc/resolv.conf &&
+sudo -- bash -c "
+    echo \"nameserver 1.1.1.1\" > /etc/resolv.conf &&
     openvpn \
-    	--config		/home/joaosantanna/.profile.ovpn \
-    	--auth-user-pass	/home/joaosantanna/.profile.secret'
+        --config         \"${HOME}/.profile.ovpn\" \
+        --auth-user-pass \"${HOME}/.profile.secret\"
+"
