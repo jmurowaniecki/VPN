000633f2da831aa6c960bd18b6f38c20f9f96356
John Murowaniecki <joao.santanna@foxbit.com.br>
2022-03-18 11:43:48 -0300

    :book: adding some steps to config and some documentation

--- daemon.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-sudo -- bash -c "
-    echo \"nameserver 1.1.1.1\" > /etc/resolv.conf &&
-    openvpn \
-        --config         \"${HOME}/.profile.ovpn\" \
-        --auth-user-pass \"${HOME}/.profile.secret\"
-"
